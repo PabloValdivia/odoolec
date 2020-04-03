d2ad7213673beefe69312a9e4987c8f68cc91c0c
Orlando Curieles <ocurieles@Orlandos-MacBook-Pro.local>
2020-04-03 18:33:29 -0500

    https://github.com/ocurieles/odoolec/issues/1

--- __manifest__.py
+++ __manifest__.py
@@ -31,6 +31,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/company.xml',
         'data/payertype_data.xml',
     ],
     # only loaded in demonstration mode
--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import taxid_type
 from . import taxpayer_type
+from . import company
