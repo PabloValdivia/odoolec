44bd2c512edae75f25535cdd5e39189a2f9e4259
jorge <jorgepr0110@gmail.com>
2020-06-12 19:07:41 -0500

    PDM-124#time 2h#modification of the form of res_partner and validation of duplicates in identification number

--- l10n_ec_ein/models/partner.py
+++ l10n_ec_ein/models/partner.py
@@ -7,3 +7,14 @@ class Partner(models.Model):
     notify_invoice = fields.Boolean('notify_invoice', default=False)
     notify_shipment = fields.Boolean('notify_shipment', default=False)
     notify_withholding = fields.Boolean('notify_withholding', default=False)
+
+    @api.constrains('vat')
+    def validate_tax_code(self):
+        if not self.parent_id:
+            for row in self:
+                vat_bp = [x.id for x in self.search(
+                    [('vat', '=', row.vat)])]
+                vat_bp.remove(row.id)
+                if len(vat_bp) >= 1:
+                    raise UserError('You are doubling the identification number')
+
