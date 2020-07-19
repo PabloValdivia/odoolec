595baf6d96bf276d2e93c9bfd61eeffcf5796cc7
carlos vizcaya <commit@ingeint.com>
2020-07-18 20:55:02 -0500

    ODOOEC-39 #CHANGE OF MODULE NAME

--- l10n_ec_ats/__manifest__.py
+++ l10n_ec_ats/__manifest__.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_ec_purchase",
+    'name': "Ecuador Localization",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This is a contribution to localize odoo in Ecuador """,
 
     'description': """
-        Long description of module's purpose
+        Tax Payer Types
+        Tax ID Types
+        RUC Validation
+        Standard Ecuador fields
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Orlando Curieles - Osiris Roman",
+    'website': "http://www.ingeint.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Localization',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/purchase.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
