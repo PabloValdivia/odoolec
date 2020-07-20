bd5186ee40cfed57ca350ec97c6d98d22f007821
carlos vizcaya <commit@ingeint.com>
2020-07-20 09:02:38 -0500

    add attribute in xml view

--- l10n_ec_ats/__manifest__.py
+++ l10n_ec_ats/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
+    'depends': ['base', 'purchase'],
 
     # always loaded
     'data': [
