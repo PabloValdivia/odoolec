4833677bee38affb50b27849388b2c9f632c0e40
carlos vizcaya <commit@ingeint.com>
2020-09-02 15:06:16 -0500

    change

--- l10n_ec_ats/models/ats.py
+++ l10n_ec_ats/models/ats.py
@@ -1,8 +1,12 @@
 from odoo import fields, models, api
-
+from datetime import datetime
 
 class Ats(models.Model):
     _name = 'ats.ec'
 
     Company_ids = fields.Many2one('res.company', default=lambda self: self.env['res.company']._company_default_get('account.invoice'))
-    month_date = fields.Date()
+    month = fields.Selection(
+        [('1', 'Ene'), ('2', 'Feb'), ('3', 'Mar'), ('4', 'Abr'), ('5', 'May'), ('6', 'Jun'), ('7', 'Jul'),
+                    ('8', 'Ago'), ('9', 'Sep'), ('10', 'Oct'), ('11', 'Nov'), ('12', 'Dic')]
+    )
+    year = fields.Integer('Year', required=True, default=datetime.today().strftime('%Y'))
