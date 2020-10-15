e01918efa62b9d93451ae545441dd3f38616ec10
carlos vizcaya <commit@ingeint.com>
2020-10-15 10:43:42 -0500

    change

--- l10n_ec_par/__manifest__.py
+++ l10n_ec_par/__manifest__.py
@@ -30,6 +30,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/groups.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/company.xml',
